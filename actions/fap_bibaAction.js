43bf45851c18c52dea32b1168fa2a134186a2a98
Bs01-1 <kkutukov.ilya@mail.ru>
2020-08-08 00:55:40 +0500

    bugfix/BIBABOT-7: -

--- actions/fap_bibaAction.js
+++ actions/fap_bibaAction.js
@@ -9,7 +9,7 @@ exports.index = async (occasion) => {
 
   let fap_cof = ( Math.round(user.biba) / 100 + (user.count_fap / 200));
   fap_cof = fap_cof < 0.4 ? 0.4 : fap_cof;
-  let add_dick = Math.round( ( random.int(100, 200) / 100 ))+ fap_cof;
+  let add_dick = (Math.round( ( random.int(100, 200) / 100 ))+ fap_cof).toFixed(2);
 
 
   user.event_id = null;
@@ -22,7 +22,7 @@ exports.index = async (occasion) => {
       first_name: vk_user[0].first_name,
       last_name: vk_user[0].last_name,
       id: user.vk_id,
-      dick: Math.abs(add_dick),
+      dick: add_dick,
       fap: 'i'
     }), occasion.peer_id);
   }
@@ -36,7 +36,7 @@ exports.index = async (occasion) => {
 
     let fap_cof = ( Math.round(user.biba) / 150 );
     fap_cof = fap_cof < 0.13 ? 0.13 : fap_cof;
-    let add_dick = Math.round( ( random.int(50, 100) / 100 )) + fap_cof;
+    let add_dick = (Math.round( ( random.int(50, 100) / 100 )) + fap_cof).toFixed(2);
 
     friend.biba += add_dick;
     friend.save();
